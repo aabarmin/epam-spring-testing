bb26e7ca5b14551cfad134a3ed74c0005c8e516f
Aleksandr Barmin <Aleksandr.Barmin@thomsonreuters.com>
2020-06-13 17:37:22 +0300

    Initial commit

---
+++ src/main/java/com/epam/community/z/springtesting/SpringTestingApplication.java
@@ -0,0 +1,13 @@
+package com.epam.community.z.springtesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringTestingApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/epam/community/z/springtesting/SpringTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.community.z.springtesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTestingApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
