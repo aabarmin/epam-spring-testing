08620b1013f818565277909b4eca7aae7c829405
Aleksandr Barmin <barmin.alexander@gmail.com>
2020-12-18 10:10:39 +0300

    Commented contract tests for a while

---
+++ external-post-generator/src/test/resources/contracts/shouldProvideAllPosts.groovy
@@ -0,0 +1,16 @@
+package contracts
+
+org.springframework.cloud.contract.spec.Contract.make {
+    request {
+        description("If all the posts are requested, they should be returned")
+        method :POST()
+        url "/posts"
+    }
+    response {
+        status OK()
+        body()
+        headers(
+                'Content-Type', producer('application/json')
+        )
+    }
+}
